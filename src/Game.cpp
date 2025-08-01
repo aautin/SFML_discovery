b0f9632e181750d07c803679c954d72121895494
aautin <alexandreautin.aa@gmail.com>
2025-08-01 14:27:09 +0200

    add maps and set window on fullscreen fixed-size

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,9 +1,11 @@
 #include "Game.hpp"
 
 Game::Game(std::string title, std::string mapFilename, sf::Vector2f tileSize)
-: _map(tileSize, mapFilename), _tileSize(tileSize), _player(),
-  _window()
+: _map(mapFilename), _tileSize(tileSize), _window()
 {
+	_window.create(sf::VideoMode::getDesktopMode(), title,
+		sf::Style::Titlebar | sf::Style::Close);
+	_window.setPosition(sf::Vector2i(0, 0));
 	_window.setVerticalSyncEnabled(false);
 }
 
@@ -23,7 +25,6 @@ void Game::events()
 		else if (event->getIf<sf::Event::KeyPressed>() || event->getIf<sf::Event::KeyReleased>()) {
 			const auto& code = event->getIf<sf::Event::KeyPressed>() ?
 				event->getIf<sf::Event::KeyPressed>()->code : event->getIf<sf::Event::KeyReleased>()->code;
-
 			if (_player.isInput(code))
 				_player.event(*event);
 		}
@@ -51,3 +52,4 @@ sf::Vector2f Game::getTileSize() const
 {
 	return _tileSize;
 }
+
